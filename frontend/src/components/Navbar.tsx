19093f8a8fc2b50b43bf47a703007ccbdf7fa00d
Kaustav Das <kaustavdas0228@gmail.com>
2025-03-26 03:03:55 +0530

    logo add

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,7 +10,6 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-import {Logo} from "./Assests/logo.png";
 interface NavbarProps {
   darkMode: boolean
   setDarkMode: (value: boolean) => void
@@ -48,7 +47,7 @@ const Navbar = ({ darkMode, setDarkMode, name, image }: NavbarProps) => {
           {/* Logo and Title - Always visible */}
           <div className="flex items-center space-x-3">
             <img
-              src={Logo}
+              src='./assets/logo.png'
               alt="Bawarchi.AI Logo"
               className="w-16 sm:w-20 object-contain"
             />
